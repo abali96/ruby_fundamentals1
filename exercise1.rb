a73a346510f977a4c08230f29fc3c273b305a13b
Arjun <arjun.bali96@gmail.com>
2014-06-03 14:14:12 -0400

    First line of ex3 written

--- exercise1.rb
+++ exercise1.rb
@@ -1,3 +1,8 @@
 puts 2 
 puts 3 
 puts 2 != 3
+
+a = 2
+b = 3
+
+puts "This is the sum: #{a+b}"
---
+++ exercise2.rb
@@ -0,0 +1,18 @@
+#Questoin 1
+puts "Question 1:"
+tip = 55*0.15
+puts tip
+
+#Question 2
+puts "Question 2:"
+#solution to question 2
+sum = "string" + 8.to_s
+puts sum
+
+#Question 3
+puts "Question 3:"
+puts "I can output the value of 45628 times 7839 in a sentence by using string interpolation like this: #{45628*7839}"
+
+#Question 4
+puts "Question 4:"
+puts "(true && false) || (false && true) || !(false && false) evaluates to true."
